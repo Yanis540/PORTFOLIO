380880ff937933f88d331ffc98cd0731e0f12bba
Yanis540 <yanistabellout4@gmail.com>
2023-07-01 22:23:54 +0100

    updated some stuff

--- installation.bat
+++ installation.bat
@@ -1,9 +1,2 @@
-npm install --save-dev tailwind-scrollbar
-npm install styled-components@5.3.10 @types/styled-components
-npm i animejs react-helmet
-npm i --save-dev @types/animejs
-npm i --save-dev @types/react-helmet 
-npm install react-intersection-observer --save
-npm i react-scroll 
-npm i scrollreveal
-npm i --save-dev @types/scrollreveal
+cd frontend 
+npm install 
