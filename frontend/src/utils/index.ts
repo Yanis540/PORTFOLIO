1f65334fffefc28a75457b2007228c16a1ab38ac
Yanis540 <yanistabellout4@gmail.com>
2023-12-13 01:49:11 +0100

    modified the animation duration

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,6 +1,6 @@
 export const navDelay = 1000;
 export const lottieLoaderAnimationDelay = 5000;// ms
-export const logoLoaderAnimationDelay= 1000+2000 
+export const logoLoaderAnimationDelay= 500 +500 // old : 1000+2000 but it was too slow
 /*
 !   -   1000ms  (hexagon) 
 !   -   2000ms  (logo-y  transition+delay) 
